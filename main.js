36e1939b84dfbe4b9d072ea0da9b6b206ce2ac17
mmd-rahimi <mohamad8rahimi3@gmail.com>
2024-11-03 14:10:13 +0330

    V_3

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 import "./style.css";
+
+const mainBar = document.getElementById("mainBar")
+const nameInput = document.getElementById("nameInput")
+const phoneNumberInput = document.getElementById("phoneNumberInput")
+const addBtn = document.getElementById("addBtn")
+
+addBtn.addEventListener("click", onClick)
+
+function onClick() {
+let li = document.createElement('li')
+li.appendChild(document.createTextNode(nameInput.value + " " + phoneNumberInput.value));
+mainBar.appendChild(li)
+}
+
